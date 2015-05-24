846650c20509ef37925ac68da1440c1f3ee6a9e7
Etienne Miret <etienne.miret@ens-lyon.org>
2015-05-24 19:47:31 +0200

    Exercice 8 : Mise en place du module ngRoute.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 'use strict';
 
-var app = angular.module('contacts', ['controllers']);
+var app = angular.module('contacts', ['ngRoute', 'controllers']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/list.html',
+            controller: 'ContactsListCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
