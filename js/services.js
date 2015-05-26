83dbf137ba58710450ccdb49adf1db59452b5ff0
Etienne Miret <etienne.miret@ens-lyon.org>
2015-05-26 14:23:04 +0200

    Exercice 9 : Web-service interrogé avec $http.

--- js/services.js
+++ js/services.js
@@ -20,34 +20,8 @@ services.factory('uriGenerator', [function() {
     }
 }]);
 
-services.factory('Contact', ['$interval', function($interval) {
-	var delay = 100; // En milliseconde.
-
-    var database = [
-        {
-            id: 0,
-            firstName: 'Olivier',
-            lastName: 'Dupont',
-            email: 'olivier.dupont@groupehn.com',
-            tel: '01 23 45 67 89'
-        },
-        {
-            id: 1,
-            firstName:'Archibald',
-            lastName: 'Haddock',
-            email: 'capitaine.haddock@herge.be',
-            tel: '0 899 708 708'
-        },
-        {
-            id: 2,
-            firstName: 'Achile',
-            lastName: 'Talon',
-            email: 'achiletalon@polite.be',
-            tel: '09 87 65 43 21'
-        }
-    ];
-
-    var lastId = 2;
+services.factory('Contact', ['$http', function($http) {
+	var baseUrl = 'http://localhost/tpangularjs.php/';
 
     /* Copie les champs de « from » vers « to » sauf
      * ceux dont le nom commence par un ‘$’. */
@@ -60,75 +34,61 @@ services.factory('Contact', ['$interval', function($interval) {
     }
 
     var Contact = function() {
-        var new_ = true;
-        var c = {};
-        var self = this;
-
-        this.$setData = function(data, full) {
-        	c = data;
-            if (full) {
-            	copyFields(c, this);
-            } else {
-                this.id = c.id;
-                this.firstName = c.firstName;
-                this.lastName = c.lastName;
-            }
-            new_ = false;
-        };
+    	var self = this;
 
         this.$save = function(params, success, error) {
-        	$interval(function() {
-        		copyFields(self, c);
-                if (new_) {
-                    c.id = ++lastId;
-                    database.push(c);
-                    self.id = c.id;
-                    new_ = false;
-                }
-                success && success();
-        	}, delay, 1);
+        	$http.post(baseUrl + (self.id ? self.id : ''), self)
+        		.success(function(data) {
+        			copyFields(data, self);
+        			success && success();
+        		})
+        		.error(function(data) {
+        			console.log('Error while saving.', self, data);
+        			error && error();
+        		});
         };
 
         this.$delete = function(params, success, error) {
-        	$interval(function() {
-        		if (new_) {
-        			error && error();
-        		} else {
-        			database.splice(database.indexOf(c), 1);
+        	$http.delete(baseUrl + (self.id ? self.id : ''))
+        		.success(function() {
         			success && success();
-        		}
-        	}, delay, 1);
+        		})
+        		.error(function(data) {
+        			console.log('Error while deleting.', self, data);
+        			error && error();
+        		});
         };
     };
 
     Contact.query = function(params, success, error) {
         var result = [];
-        $interval(function() {
-            for (var i = 0; i < database.length; i++) {
-            	var c = new Contact();
-            	c.$setData(database[i], false);
-                result.push(c);
-            }
-        }, delay, 1);
+        $http.get(baseUrl)
+        	.success(function(data) {
+        		for (var i = 0; i < data.length; i++) {
+        			var c = new Contact();
+        			copyFields(data[i], c);
+        			result.push(c);
+        		}
+        		success && success();
+        	})
+        	.error(function(data) {
+        		console.log('Error while querying all.', data);
+        		error && error();
+        	});
         return result;
     };
 
     Contact.get = function(params, success, error) {
     	var result = new Contact();
-    	$interval(function() {
-            var i, c;
-            for (i = 0; i < database.length; i++) {
-                c = database[i];
-                if (c.id == params.id) {
-                    break;
-                }
-            }
-            if (i == database.length) {
-                error && error();
-            } else {
-            	result.$setData(c, true);
-            }
-    	}, delay, 1);
+    	$http.get(baseUrl + params.id)
+    		.success(function(data) {
+    			copyFields(data, result);
+    			success && success();
+    		})
+    		.error(function(data) {
+    			console.log('Error while fetching ' + params.id, data);
+    			error && error();
+    		});
         return result;
     }
 
