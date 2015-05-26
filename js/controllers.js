284f7bcb2aa2430d94374c9871d02ca4468dd552
Etienne Miret <etienne.miret@ens-lyon.org>
2015-05-26 12:38:28 +0200

    Exercice 8 : Nettoyage du controleur de la page d'accueil.

--- js/controllers.js
+++ js/controllers.js
@@ -3,28 +3,7 @@
 var controllers = angular.module('controllers', ['services']);
 
 controllers.controller('ContactsListCtrl', ['$scope', 'Contact', function($scope, Contact) {
-
     $scope.contacts = Contact.query();
-
-    $scope.remove = function(contact) {
-        contact.$delete({}, function() {
-            $scope.contacts = Contact.query();
-        });
-    };
-
-    $scope.newContact = new Contact();
-
-    $scope.add = function() {
-        if ($scope.newContact.firstName
-                && $scope.newContact.lastName
-                && $scope.newContact.email
-                && $scope.newContact.tel) {
-            $scope.newContact.$save({}, function() {
-                $scope.newContact = new Contact();
-                $scope.contacts = Contact.query();
-            });
-        }
-    };
 }]);
 
 controllers.controller('ContactsDetailCtrl', ['$scope', '$location', '$routeParams', 'Contact', 'uriGenerator', function ($scope, $location, $routeParams, Contact, uriGenerator) {
