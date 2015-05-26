a4f89a0eb7226a582b9779a240b609e73865bd01
Etienne Miret <etienne.miret@ens-lyon.org>
2015-05-26 12:34:30 +0200

    Exercice 8 : Utilisation du service asynchrone.

--- js/controllers.js
+++ js/controllers.js
@@ -28,26 +28,30 @@ controllers.controller('ContactsListCtrl', ['$scope', 'Contact', function($scope
 }]);
 
 controllers.controller('ContactsDetailCtrl', ['$scope', '$location', '$routeParams', 'Contact', 'uriGenerator', function ($scope, $location, $routeParams, Contact, uriGenerator) {
-    $scope.contact = Contact.get($routeParams);
-    $scope.contact.telUri = uriGenerator.tel($scope.contact.tel);
+    $scope.contact = Contact.get($routeParams, function() {
+        $scope.contact.telUri = uriGenerator.tel($scope.contact.tel);
+    });
     $scope.delete = function() {
-        $scope.contact.$delete();
-        $location.path('/');
+        $scope.contact.$delete({}, function() {
+            $location.path('/');
+        });
     };
 }]);
 
 controllers.controller('ContactsEditCtrl', ['$scope', '$location', '$routeParams', 'Contact', function($scope, $location, $routeParams, Contact) {
     $scope.contact = Contact.get($routeParams);
     $scope.save = function() {
-        $scope.contact.$save();
-        $location.path('/' + $scope.contact.id);
+        $scope.contact.$save({}, function() {
+            $location.path('/' + $scope.contact.id);
+        });
     };
 }]);
 
 controllers.controller('ContactsNewCtrl', ['$scope', '$location', 'Contact', function($scope, $location, Contact) {
     $scope.contact = new Contact();
     $scope.save = function() {
-        $scope.contact.$save();
-        $location.path('/' + $scope.contact.id);
+        $scope.contact.$save({}, function() {
+            $location.path('/' + $scope.contact.id);
+        });
     };
 }]);
