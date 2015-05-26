8a6ff38e41161475ac5cf25b6c0db296b9703944
Etienne Miret <etienne.miret@ens-lyon.org>
2015-05-26 17:10:37 +0200

    Exercice 11 : Utilisation de la directive hnLoading.

--- js/controllers.js
+++ js/controllers.js
@@ -3,14 +3,21 @@
 var controllers = angular.module('controllers', ['services']);
 
 controllers.controller('ContactsListCtrl', ['$scope', 'Contact', function($scope, Contact) {
-    $scope.contacts = Contact.query();
+    $scope.listIsLoading = true;
+    $scope.contacts = Contact.query({}, function() {
+        $scope.listIsLoading = false;
+    });
 }]);
 
 controllers.controller('ContactsDetailCtrl', ['$scope', '$location', '$routeParams', 'Contact', 'uriGenerator', function ($scope, $location, $routeParams, Contact, uriGenerator) {
+    $scope.contactIsLoading = true;
     $scope.contact = Contact.get($routeParams, function() {
+        $scope.contactIsLoading = false;
         $scope.contact.telUri = uriGenerator.tel($scope.contact.tel);
     });
+    $scope.deleting = false;
     $scope.delete = function() {
+        $scope.deleting = true;
         $scope.contact.$delete({}, function() {
             $location.path('/');
         });
@@ -18,8 +25,13 @@ controllers.controller('ContactsDetailCtrl', ['$scope', '$location', '$routePara
 }]);
 
 controllers.controller('ContactsEditCtrl', ['$scope', '$location', '$routeParams', 'Contact', function($scope, $location, $routeParams, Contact) {
-    $scope.contact = Contact.get($routeParams);
+    $scope.contact = Contact.get($routeParams, function() {
+        $scope.loading = false;
+    });
+    $scope.loading = true;
+    $scope.saving = false;
     $scope.save = function() {
+        $scope.saving = true;
         $scope.contact.$save({}, function() {
             $location.path('/' + $scope.contact.id);
         });
@@ -28,7 +40,10 @@ controllers.controller('ContactsEditCtrl', ['$scope', '$location', '$routeParams
 
 controllers.controller('ContactsNewCtrl', ['$scope', '$location', 'Contact', function($scope, $location, Contact) {
     $scope.contact = new Contact();
+    $scope.loading = false;
+    $scope.saving = false;
     $scope.save = function() {
+        $scope.saving = true;
         $scope.contact.$save({}, function() {
             $location.path('/' + $scope.contact.id);
         });
