f69cce8b9f86f4cc36a6739e4b3f836d335f597d
Etienne Miret <etienne.miret@ens-lyon.org>
2015-05-27 11:02:51 +0200

    Exercice 12 : Création du filtre relativeTime.

---
+++ js/filters.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var filters = angular.module('filters', []);
+
+filters.filter('relativeTime', ['dateFilter', function(dateFilter) {
+    return function(input, now) {
+        var result;
+        if (input) {
+            var date = new Date(input);
+            var difference = now.getTime() - date.getTime(); // Écart en millisecondes.
+            if (difference < 2000) { // Moins de 2 secondes.
+                result = '\u00c0 l\u2019instant.';
+            } else if (difference < 60 * 1000) { // Moins d’une minute.
+                result = 'Il y a ' + Math.floor(difference / 1000) + ' secondes.';
+            } else if (difference < 120 * 1000) { // Moins de deux minutes.
+                result = 'Il y a une minute.';
+            } else if (difference < 3600 * 1000) { // Moins d’une heure.
+                result = 'Il y a ' + Math.floor(differnce / 60000) + ' minutes.';
+            } else if (now.getFullYear() == date.getFullYear() && now.getMonth() == date.getMonth() && now.getDate() == date.getDate()) { // Aujourd’hui.
+                result = 'Aujourd\u2019hui \u00e0 ' + dateFilter(date, "HH'h'mm");
+            } else {
+                result = dateFilter(date, "'Le 'dd'/'MM'/'yyyy' \u00e0 'HH'h'mm");
+            }
+        } else {
+            result = '';
+        }
+        return result;
+    }
+}]);
