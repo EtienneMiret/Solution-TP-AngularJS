7058b41e2dc92c40fae095ba516dcf4636eb87c2
Etienne Miret <etienne.miret@ens-lyon.org>
2015-05-28 11:16:00 +0200

    Exercice 14 : Création de tests pour ContactsDetailCtrl.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,26 +1,43 @@
 'use strict';
 
-describe('Les contrôleurs de contacts', function() {
+describe('The controller', function() {
+    var scope, createController, httpBackend;
     var webserviceUrl = 'http://vip46.groupehn.com:20003/angularjs/tpangularjs.php';
 
     beforeEach(function() {
         jasmine.addMatchers({
             toEqualData: function() {
                 return {
-                    compare: function(expected, actual) {
+                    compare: function(actual, expected) {
                         return {
                             pass: angular.equals(expected, actual)
                         };
                     }
                 };
+            },
+            toEqualNow: function() {
+                return {
+                    compare: function(actual) {
+                        return {
+                            pass: actual instanceof Date && Math.abs((new Date()).getTime() - actual.getTime()) < 100
+                        }
+                    }
+                }
             }
         });
     });
 
     beforeEach(module('contacts'));
 
+    beforeEach(inject(function($httpBackend, $rootScope, $controller) {
+        httpBackend = $httpBackend;
+        scope = $rootScope.$new();
+        createController = function(ctrlName) {
+            return $controller(ctrlName, {$scope: scope});
+        };
+    }));
+
     describe('ContactsListCtrl', function() {
-        var scope, createController, httpBackend;
         var data = [
             {
                 id: 1,
@@ -39,17 +56,9 @@ describe('Les contrôleurs de contacts', function() {
             }
         ];
 
-        beforeEach(inject(function($httpBackend, $rootScope, $controller) {
-            httpBackend = $httpBackend;
-            scope = $rootScope.$new();
-            createController = function() {
-                return $controller('ContactsListCtrl', {$scope: scope});
-            };
-        }));
-
         it('should initialy be loading', function() {
             httpBackend.expectGET(webserviceUrl).respond(data);
-            createController();
+            createController('ContactsListCtrl');
             expect(scope.listIsLoading).toBe(true);
             expect(scope.contacts).toEqualData([]);
             expect(scope.msg).toBe(undefined);
@@ -58,7 +67,7 @@ describe('Les contrôleurs de contacts', function() {
 
         it('should properly load contacts', function() {
             httpBackend.expectGET(webserviceUrl).respond(data);
-            createController();
+            createController('ContactsListCtrl');
             httpBackend.flush();
             expect(scope.listIsLoading).toBe(false);
             expect(scope.contacts).toEqualData(data);
@@ -67,18 +76,103 @@ describe('Les contrôleurs de contacts', function() {
 
         it('should gracefully handle failures', function() {
             httpBackend.expectGET(webserviceUrl).respond(500, 'Server is down');
-            createController();
+            createController('ContactsListCtrl');
             httpBackend.flush();
             expect(scope.listIsLoading).toBe(false);
             expect(scope.contacts).toEqualData([]);
             expect(scope.msg).toBe('Server is down');
         });
 
-        afterEach(function() {
-            httpBackend.verifyNoOutstandingExpectation();
-            httpBackend.verifyNoOutstandingRequest();
+    });
+
+    describe('ContactsDetailCtrl', function() {
+        var url = webserviceUrl + '/1';
+        var haddock = {
+            id: 1,
+            firstName: 'Archibald',
+            lastName: 'Haddock',
+            email: 'capitaine.haddock@herge',
+            tel: '00 32 2 36 42 12',
+            creation: '2015-05-27T10:20:16+02:00',
+            modification: '2015-05-27T10:20:16+02:00',
+            fields: []
+        };
+
+        beforeEach(inject(function($routeParams) {
+            $routeParams.id = '1';
+        }));
+
+        it('should initialy be loading', function() {
+            httpBackend.expectGET(url).respond(haddock);
+            createController('ContactsDetailCtrl');
+            expect(scope.contactIsLoading).toBe(true);
+            expect(scope.deleting).toBe(false);
+            expect(scope.contact).toEqualData({});
+            expect(scope.msg).toBe(undefined);
+            expect(scope.now).toEqualNow();
+            expect(scope.delete).toEqual(jasmine.any(Function));
+            httpBackend.flush();
         });
 
+        it('should properly load one contact', function() {
+            httpBackend.expectGET(url).respond(haddock);
+            createController('ContactsDetailCtrl');
+            httpBackend.flush();
+            expect(scope.contactIsLoading).toBe(false);
+            expect(scope.deleting).toBe(false);
+            expect(scope.contact).toEqualData(haddock);
+            expect(scope.msg).toBe(undefined);
+            expect(scope.now).toEqualNow();
+            expect(scope.delete).toEqual(jasmine.any(Function));
+        });
+
+        it('should gracefully handle failures', function() {
+            httpBackend.expectGET(url).respond(404, 'Not found.');
+            createController('ContactsDetailCtrl');
+            httpBackend.flush();
+            expect(scope.contactIsLoading).toBe(false);
+            expect(scope.deleting).toBe(false);
+            expect(scope.contact).toEqualData({});
+            expect(scope.msg).toBe('Not found.');
+            expect(scope.now).toEqualNow();
+            expect(scope.delete).toEqual(jasmine.any(Function));
+        });
+
+        it('should be able to delete a contact', function() {
+            httpBackend.expectGET(url).respond(haddock);
+            createController('ContactsDetailCtrl');
+            httpBackend.flush();
+            httpBackend.expectDELETE(url).respond('Contact deleted.');
+            scope.delete();
+            expect(scope.contactIsLoading).toBe(false);
+            expect(scope.deleting).toBe(true);
+            expect(scope.contact).toEqualData(haddock);
+            expect(scope.msg).toBe(undefined);
+            expect(scope.now).toEqualNow();
+            expect(scope.delete).toEqual(jasmine.any(Function));
+            httpBackend.flush();
+        });
+
+        it('should gracefully handle deletion failures', function() {
+            httpBackend.expectGET(url).respond(haddock);
+            createController('ContactsDetailCtrl');
+            httpBackend.flush();
+            httpBackend.expectDELETE(url).respond(403, 'Deletion not allowed.')
+            scope.delete();
+            httpBackend.flush();
+            expect(scope.contactIsLoading).toBe(false);
+            expect(scope.deleting).toBe(false);
+            expect(scope.contact).toEqualData(haddock);
+            expect(scope.msg).toBe('Deletion not allowed.');
+            expect(scope.now).toEqualNow();
+            expect(scope.delete).toEqual(jasmine.any(Function));
+        });
+
+    });
+
+    afterEach(function() {
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
     });
 
 });
